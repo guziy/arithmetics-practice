a3a865c5783f5b962283f4c4e2e17bac84546ef7
huziy <guziy.sasha@gmail.com>
2020-01-25 09:27:03 -0500

    updates for logic

--- js/arithmetics.js
+++ js/arithmetics.js
@@ -1,6 +1,33 @@
 var app = new Vue({
   el: '#app',
   data: {
-    message: 'Hello Vue!'
+    message: 'Entrez votre nom SVP:',
+    name: '',
+
+    n_total: 20,
+    // triples of numbers
+    triples: [],
+    // index of the field 0,1 or 2
+    field_index: []
+  },
+  created: function(){
+    const MAX_NUM = 100;
+    var i;
+    var num1, num2, num3;
+    var additon;
+    var r; // from [0, 1)
+    for (i = 0; i < this.n_total; i++) {
+      console.log(i);
+
+      r = Math.random();
+
+      additon = r > 0.5;
+
+      num1 = 0;
+
+      triples.append([]);
+
+    }
+
   }
-})
+});
