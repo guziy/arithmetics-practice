342b6790c9c7da85757eaf684f7d2522ad968f9a
huziy <guziy.sasha@gmail.com>
2020-02-07 07:43:23 -0500

    equation component

---
+++ src/components/equation.vue
@@ -0,0 +1,177 @@
+<script>
+
+momentDurationFormatSetup(moment);
+
+/** initial state of the equation component comp
+*/
+function equation_initial_state() {
+
+  return {
+    visible: false,
+    tokens: [],
+    user_answer: "",
+    corrected: false,
+    is_correct: false,
+    warning_message: "",
+    correct_answer: -1,
+  };
+}
+
+
+
+export default {
+
+    name: "equation",
+
+    props:["equation_data", "event_bus",
+           'n_tried'],
+    data: function(){
+      return equation_initial_state();
+    },
+    template: "#equation",
+
+    created: function() {
+      // this.event_bus.$on("init-equations", this.init_equation);
+      this.init_equation();
+
+      // listen to the event bus, to focus next equation input on correct
+      this.event_bus.$on("focus-equation", (event) => {
+        //console.log(this.equation_data.equation_index + "-- received --> " + event);
+        if (event.src_index + 1 == this.equation_data.equation_index) {
+          this.$nextTick(
+            () => {
+                if (this.is_visible()){
+                    this.$refs.input[0].focus();
+                }
+            });
+        }
+      }); // this.event_bus.$on
+    },
+
+    mounted: function () {
+    },
+
+    methods: {
+      on_correct: function (event) {
+        //TODO: add action
+
+        if (this.user_answer === "") {
+          console.log("Nothing was entered");
+          return
+        }
+
+        this.is_correct = this.correct_answer == this.user_answer;
+
+        event = {
+          correct: this.is_correct,
+
+        };
+
+        this.$emit("oncorrect-equation", event);
+        this.event_bus.$emit("focus-equation", {src_index: this.equation_data.equation_index});
+        this.corrected = true;
+      },
+      is_visible: function() {
+        return this.equation_data.equation_index <= this.n_tried;
+      },
+
+      get_equation_tokens: function() {
+        var tokens = this.equation_data.triple.slice();
+        tokens.splice(2, 0, '=');
+        tokens.splice(1, 0, this.equation_data.operation);
+
+        return tokens
+      },
+
+      init_equation: function () {
+        var nums = this.equation_data.triple;
+        //console.log("init_equation with " + nums.join(",") );
+
+        if (this.equation_data.operation === "+") {
+          switch (this.equation_data.input_index) {
+            case 0:
+              this.correct_answer = nums[2] - nums[1];
+              break;
+
+            case 1:
+              this.correct_answer = nums[2] - nums[0];
+              break;
+
+            case 2:
+              this.correct_answer = nums[0] + nums[1];
+              break;
+          }
+        } else if (this.equation_data.operation === "-") {
+          switch (this.equation_data.input_index) {
+            case 0:
+              this.correct_answer = nums[1] + nums[2];
+              break;
+
+            case 1:
+              this.correct_answer = nums[0] - nums[2];
+              break;
+
+            case 2:
+              this.correct_answer = nums[0] - nums[1];
+              break;
+          }
+        } else {
+          console.log("operation: " + this.operation + "is not implemented.");
+        }
+        this.corrected = false;
+        this.correct = false;
+        this.user_answer = "";
+
+        this.tokens = this.get_equation_tokens();
+        this.visible = this.is_visible();
+
+        // ficus the input if required
+        this.$nextTick(
+          () => {
+              if (this.is_visible()){
+                  this.$refs.input[0].focus();
+              }
+          }
+        );
+
+      },
+
+    }, // methods
+    watch: {
+      equation_data: function(newVal, oldVal){
+        this.init_equation();
+      },
+    }
+}
+
+</script>
+
+<template id="equation">
+  <div class="row justify-content-left">
+
+    <div class="col-6 p-2">
+      <span v-for="token in tokens">
+        <span v-if="token === -1">
+          <input size="4"  v-model.number="user_answer" v-on:keyup.enter="on_correct()" :disabled="corrected" ref="input"/>
+        </span>
+        <span v-else>
+          {{token}}
+        </span>
+      </span>
+    </div>
+
+    <div v-if="!corrected" class="p-2">
+      <button type="button" @click="on_correct()">Corriger!</button>
+      <i class="fa fa-spinner fa-spin"></i>
+    </div>
+    <div v-if="corrected && is_correct" class="text-success p-2">
+      <i class="fa fa-check"></i>
+      Bien joué!
+    </div>
+    <div v-if="corrected && !is_correct" class="text-danger p-2">
+        <i class="fa fa-times"></i>
+        La reponse est: {{correct_answer}}.
+    </div>
+
+  </div>
+</template>
